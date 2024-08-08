f15b47abf47782805a466b5f1d9334e8c268df65
Michael <mikey.smith39@gmail.com>
2024-08-08 16:25:58 -0700

    Update
    fixed Carousel not working issue

--- app/static/js/Carousel.js
+++ app/static/js/Carousel.js
@@ -1,11 +1,11 @@
 let currentIndex = 0;
-const images = document.querySelectorAll('.carousel-images img');
+const images = document.querySelectorAll(".dashboard-carousel-images img"); // Updated to match HTML class
 const totalImages = images.length;
 
 function showNextImage() {
-  images[currentIndex].style.display = 'none';
+  images[currentIndex].style.display = "none";
   currentIndex = (currentIndex + 1) % totalImages;
-  images[currentIndex].style.display = 'block';
+  images[currentIndex].style.display = "block";
 }
 
 setInterval(showNextImage, 3000); // Change image every 3 seconds
